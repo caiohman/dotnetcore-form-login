dec0523fa54be80a4269ace279f22b9a6886457e
caiohman <4341494f@usp.br>
2020-07-08 08:04:39 -0300

    base

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dotnet_form_test
+{
+    public partial class Form1 : Form
+    {
+        private TableLayoutPanel tableLayout;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tableLayout = tableLayoutStart();
+            this.Controls.Add(tableLayout);
+        }
+
+        private TextBox userTextBox()
+        {
+          TextBox textBox = new TextBox();
+
+          return textBox;
+        }
+
+        private Button loginButton()
+        {
+          Button button = new Button();
+          button.Height = 50;
+          button.Width = 150;
+          button.Text = "Login";
+          button.Font = new Font("Comic Sans MS" , 12);
+          button.BackColor = Color.Blue;
+          button.ForeColor = Color.White; //text's color
+          button.Padding = new Padding(6);
+
+          return button;
+        }
+
+        private TableLayoutPanel tableLayoutStart()
+        {
+          TableLayoutPanel layout = new TableLayoutPanel();
+          layout.BackColor = Color.Black; //debug position
+          layout.Controls.Add(loginButton(), 0, 0);
+
+          return layout;
+        }
+
+    }
+}
